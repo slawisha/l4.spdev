96cbdaa9ee97941dd06621440b86bc1d4fa81bd9
slawisha <slawisha@yahoo.com>
2014-05-06 16:44:00 +0200

    =minor changes

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -7,11 +7,6 @@ class UsersTableSeeder extends Seeder {
 		// Uncomment the below to wipe the table clean before populating
 		 DB::table('users')->truncate();
 
-		$users = array(
-			'username' => 'slawisha',
-			'email' => 'slawisha@yahoo.com',
-			'password' => Hash::make('arpeggio')
-		);
 
 		// Uncomment the below to run the seeder
 		 DB::table('users')->insert($users);
